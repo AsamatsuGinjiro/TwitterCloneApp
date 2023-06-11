fc676042030070f64d9e706fe0762bf0a36ecd84
AsamatsuGinjiro <aagin1973@gmail.com>
2023-06-12 00:13:58 +0900

    ログアウト実装

--- src/main/java/controller/Logout.java
+++ src/main/java/controller/Logout.java
@@ -6,34 +6,50 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class LogoutServlet
- */
+import model.UserDto;
+
+/**----------------------------------------------------------------------*
+ *■■■ExecuteLogoutクラス■■■
+ *概要：サーブレット
+ *詳細：HTML文書（ログアウト完了画面）を出力する。
+ *----------------------------------------------------------------------**/
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Logout() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public Logout() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		//レスポンス（出力データ）の文字コードを設定
+		response.setContentType("text/html;charset=UTF-8");  //文字コードをUTF-8で設定
+
+		//セッションからユーザーデータを取得
+		HttpSession session           = request.getSession();
+		UserDto userInfoOnSession = (UserDto)session.getAttribute("LOGIN_INFO");
+
+		//ログイン状態によって表示画面を振り分ける
+		if (userInfoOnSession != null) {
+			//ログイン済：ログアウト処理を実施
+
+			//ログアウトに伴いセッション情報を破棄
+			session.invalidate();
+
+			response.sendRedirect("Login");
+
+		} else {
+			//未ログイン：ログイン画面へ転送
+			response.sendRedirect("Login");
+		}
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
